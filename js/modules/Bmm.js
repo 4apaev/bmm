3c6e98c3ef7bfb11546e41b1f6d1595d831d63e4
michael <4apaev@gmail.com>
2014-01-12 17:40:10 +0200

    dunno

--- js/modules/Bmm.js
+++ js/modules/Bmm.js
@@ -1,6 +1,5 @@
 define('modules/Bmm', ['modules/walk', 'modules/Search'], function (walk, Search) {
 
-
   var marko = function (child) {
     var btn = child.firstElementChild;
     if(btn && btn.classList.contains('toggle') && !btn.checked) {
@@ -33,36 +32,15 @@ define('modules/Bmm', ['modules/walk', 'modules/Search'], function (walk, Search
       $.doqc('find').on('click', this.doFind.bind(this));
 
       this.updateEventHandlers(trg);
-
-      // var emptyDirs = $('#bookmarks li[data-children="0"]');
-      // emptyDirs.each(function(node) {
-      //   node.style.backgroundColor = "crimson";
-      //   marko(node);
-      // });
-      // log(emptyDirs);
-
-      // $.doqi('results').html('<ul>' + walk(res, []).join('') + '</ul>');
-      // this.updateEventHandlers();
-
-      // search.value = "flash";
-      // find[0].click();
     },
 
     showEmpty: function(trg) {
-
       $.dq("#results").html('<ul class="empty"></ul>');
-
       var
         trg  = $.dq("#results > ul"),
         frag = document.createDocumentFragment();
-
       $('#bookmarks li[data-children="0"]').each(frag.append.bind(frag));
       trg.append(frag);
-    },
-
-    updateEventHandlers: function(trg) {
-      trg = trg || $.doqi('results');
-      $.doqa('li:not([data-children]', trg).on('dblclick', this.toggleEditMode.bind(this));
     },
 
     updateEventHandlers: function(trg) {
@@ -146,54 +124,27 @@ define('modules/Bmm', ['modules/walk', 'modules/Search'], function (walk, Search
       } else {
 
         chrome.bookmarks.get(ids[1], function(bmmNode) {
-
           var twin = $('#bookmarks li[data-dir="' + dad.dataset.dir + '"]')[0];
           twin.scrollIntoView();
           twin.style.backgroundColor = "crimson";
           dad.style.backgroundColor = "crimson";
           btn.style.width = "auto";
-
           btn.txt("are you shure?");
           btn.classList.add('remove');
-
           marko(twin.parentElement.parentElement);
-
         });
       }
     },
 
-
     doFind: function() {
-
-      var val = $.doqi('search').value.trim();
-
+      var
+        sort,
+        val = $.doqi('search').value.trim();
       if(!val) return;
-      var sort = _.find($.doqn('filter'), 'checked').value, rgx  = 'or';
-
+      sort = _.find($.doqn('filter'), 'checked').value, rgx  = 'or';
       this.lastSearch = this.search.doSearch(val, sort, rgx);
       this.showSearchResults(this.lastSearch.reverse());
     }
   }
   return Bmm;
-})
-
-
-// var path = [];
-
-// var el = document.getElementById('myNode');
-
-
-
-
-// do {
-//     path.unshift(el.nodeName + (el.dataset.dir ? ' data-dir="' + el.dataset.dir + '"' : ''));
-
-// } while ((el.nodeName.toLowerCase() != 'html') && (el = el.parentNode));
-
-// function nodePath(x, res) {
-
-
-//     return res;
-//   }
-
-
+});
